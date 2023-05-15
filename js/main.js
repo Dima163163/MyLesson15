33363de8a273474bd3b53ca51e8b84090053afd0
Dima163163 <khlopovdima@gmail.com>
2023-05-15 15:59:03 +0400

    lesson15 готовый

---
+++ js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+  this.createElement = function () {
+    if (selector[0] == ".") {
+      const div = document.createElement("div");
+      div.classList.add(`${selector.split(".")[1]}`);
+      div.innerHTML = "Любой текст div";
+      document.body.append(div);
+    }
+    if (selector[0] == "#") {
+      const p = document.createElement("p");
+      p.id = `${selector}`;
+      p.innerHTML = "Любой текст p";
+      p.style.cssText = `height: ${this.height}px; width: ${this.height}px; bg: ${this.bg}px; fontSize: ${this.fontSize}px;`;
+      document.body.append(p);
+    }
+  };
+};
+
+const BlockDomElement = new DomElement(".block");
+const ParagraphDomElement = new DomElement("#best");
+BlockDomElement.createElement();
+ParagraphDomElement.createElement();
