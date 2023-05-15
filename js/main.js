1f6693b72224293893e154f727dfdb31712cdfc4
Dima163163 <khlopovdima@gmail.com>
2023-05-15 18:42:07 +0400

    lesson15

--- js/main.js
+++ js/main.js
@@ -1,25 +1,44 @@
 "use strict";
 
-const DomElement = function (selector, height, width, bg, fontSize) {
+const DomElement = function (
+  selector = ".block",
+  height = 150,
+  width = 150,
+  bg = "blue",
+  fontSize = 24
+) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
   this.createElement = function () {
-    if (selector[0] == ".") {
-      const div = document.createElement("div");
-      div.classList.add(`${selector.split(".")[1]}`);
-      div.innerHTML = "Любой текст div";
-      document.body.append(div);
+    const tag = selector[0] == "." ? "div" : "p";
+
+    const element = document.createElement(tag);
+    element.innerHTML = `Любой текст ${tag}`;
+    element.style.cssText = `height: ${this.height}px; width: ${this.height}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
+    if (element[0] == ".") {
+      element.classList.add(`${selector.split(".")[1]}`);
+    } else {
+      element.id = `${selector.split("#")[1]}`;
     }
-    if (selector[0] == "#") {
-      const p = document.createElement("p");
-      p.id = `${selector}`;
-      p.innerHTML = "Любой текст p";
-      p.style.cssText = `height: ${this.height}px; width: ${this.height}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
-      document.body.append(p);
-    }
+    document.body.append(element);
+
+    // if (selector[0] == ".") {
+    //   const div = document.createElement("div");
+    //   div.classList.add(`${selector.split(".")[1]}`);
+    //   div.innerHTML = "Любой текст div";
+    //   div.style.cssText = `height: ${this.height}px; width: ${this.height}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
+    //   document.body.append(div);
+    // }
+    // if (selector[0] == "#") {
+    //   const p = document.createElement("p");
+    //   p.id = `${selector.split("#")[1]}`;
+    //   p.innerHTML = "Любой текст p";
+    //   p.style.cssText = `height: ${this.height}px; width: ${this.height}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
+    //   document.body.append(p);
+    // }
   };
 };
 
